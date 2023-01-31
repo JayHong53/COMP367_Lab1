363414c7338e3649ca8d7815e72a001f3f7f2a12
JayHong52 <90368845+JayHong52@users.noreply.github.com>
2023-01-30 23:00:52 -0500

    initial commit

---
+++ COMP367_Lab1_WebApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP367_Lab1_WebApp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-CA"));
+        ViewData["TimeStamp"] = dateTime;
+    }
+}
+
+
